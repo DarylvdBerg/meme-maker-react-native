1839e65ce53c785855bda32864c7705108fa7a2d
Darylvdberg <darylvdberg@gmail.com>
2023-04-23 16:18:49 +0200

    Setup absolute paths, setup basic routing, added zustand and camera to project

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,24 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ['.'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            // This needs to be mirrored in tsconfig.json
+            '@components': './components',
+            '@screens': './screens',
+            '@constants': './constants',
+            '@models': './models',
+            '@helpers': './helpers',
+            '@hooks': './hooks',
+            '@store': './store', 
+          }
+        }
+      ]
+    ]
   };
 };
